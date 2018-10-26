74fcee6e7a82b79f055f283a93859b7f4021bf8f
liuweibang <913635030@qq.com>
2018-10-26 14:51:02 +0800

    first

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@Controller
+@RequestMapping
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @GetMapping
+    public String test(){
+        return "123";
+    }
+    @GetMapping("/index")
+    public String test(Model model){
+        model.addAttribute("name","99");
+        model.addAttribute("num","22");
+        return "show";
+    }
+
+}
---
+++ demo/src/main/webapp/WEB-INF/show.jsp
@@ -0,0 +1,15 @@
+<%@page contentType="text/html; charset=UTF-8" language="java" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="fmt"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/functions" prefix="fn"%>
+<html >
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <title>主页</title>
+</head>
+<body>
+${name}
+<br/>
+${num}
+</body>
+</html>
